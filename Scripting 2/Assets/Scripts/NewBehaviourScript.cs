eee9f48aac0a8a0684e667f452701e36fcfc5e4c
ZHolbrook <10994576@uvu.edu>
2024-09-11 20:21:54 -0600

    Added scripting 2
    added scripting 2 to unity folder messed with rigidbody and force

---
+++ Scripting 2/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+       private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.right * 500);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision detected with " + collision.gameObject.name);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
